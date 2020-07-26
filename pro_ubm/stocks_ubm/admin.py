7745ae01d6f93e3fb09556daefb8a4e51c6cb4ff
mhdnafees <nafees288@gmail.com>
2020-07-27 02:27:43 +0530

    delete

--- pro_ubm/stocks_ubm/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Stock,SalesRegistry,PurchaseRegistry,ProfitGen,InvoiceGen
-
-# Register your models here.
-admin.site.register(Stock)
-admin.site.register(SalesRegistry)
-admin.site.register(PurchaseRegistry)
-admin.site.register(ProfitGen)
-admin.site.register(InvoiceGen)
--- pro_ubm/stocks_ubm/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StocksUbmConfig(AppConfig):
-    name = 'stocks_ubm'
